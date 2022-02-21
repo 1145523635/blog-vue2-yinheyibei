2b13cf0a2014ac5d2f6af787ccb550cc032c3fdb
银河以北 <1145523635@qq.com>
2022-02-21 21:24:28 +0800

    删除铭感信息

--- src/components/loginBox/LoginBox.vue
+++ src/components/loginBox/LoginBox.vue
@@ -3,7 +3,7 @@
  * @Author: 银河以北
  * @Date: 2021-06-15 14:19:12
  * @LastEditors: 银河以北
- * @LastEditTime: 2021-12-09 21:25:28
+ * @LastEditTime: 2022-02-21 21:24:10
 -->
 <template>
   <el-dialog
@@ -194,7 +194,7 @@ export default {
       show: false,
       //登录表单
       loginForm: {
-        email: "1145523635@qq.com",
+        email: "",
         password: "",
       },
 
@@ -229,7 +229,7 @@ export default {
 
       //注册表单
       registerForm: {
-        email: "3627629185@qq.com",
+        email: "",
         code: undefined,
         password: undefined,
         againPassword: undefined,
